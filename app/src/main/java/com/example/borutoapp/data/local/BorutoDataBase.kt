82b8263df96403daee5bfca0d2e4b8931a2bbe48
Felipe Policisse <policisse@gmail.com>
2022-01-20 01:00:23 -0300

    .

--- app/src/main/java/com/example/borutoapp/data/local/BorutoDataBase.kt
+++ app/src/main/java/com/example/borutoapp/data/local/BorutoDataBase.kt
@@ -4,18 +4,18 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.borutoapp.data.local.dao.HeroDao
-import com.example.borutoapp.domain.model.HeroRemoteKey
-import com.example.borutoapp.data.local.dao.HeroRemoteKeyDao
+import com.example.borutoapp.domain.model.HeroRemoteKeys
+import com.example.borutoapp.data.local.dao.HeroRemoteKeysDao
 import com.example.borutoapp.domain.model.Hero
 
 @Database(
-    entities = [Hero::class, HeroRemoteKey::class],
+    entities = [Hero::class, HeroRemoteKeys::class],
     version = 1
 )
 @TypeConverters(DatabaseConverter::class)
 abstract class BorutoDataBase: RoomDatabase() {
 
     abstract fun heroDao(): HeroDao
-    abstract fun heroRemoteKeyDao(): HeroRemoteKeyDao
+    abstract fun heroRemoteKeysDao(): HeroRemoteKeysDao
 
 }
