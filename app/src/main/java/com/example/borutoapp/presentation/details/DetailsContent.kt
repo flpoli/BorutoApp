73f27440dec9fc44c111b4102fe180275ad994ef
Felipe Policisse <policisse@gmail.com>
2022-02-12 20:56:59 -0300

    .

--- app/src/main/java/com/example/borutoapp/presentation/details/DetailsContent.kt
+++ app/src/main/java/com/example/borutoapp/presentation/details/DetailsContent.kt
@@ -1,5 +1,6 @@
 package com.example.borutoapp.presentation.details
 
+import android.graphics.Color.parseColor
 import android.util.Log
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.Image
@@ -9,8 +10,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -38,9 +38,19 @@ import com.example.borutoapp.util.Constants.MIN_BACKGROUND_IMAGE_HEIGHT
 @Composable
 fun DetailsContent(
     navController: NavHostController,
-    selectedHero: Hero?
+    selectedHero: Hero?,
+    colors: Map<String, String>
 ) {
 
+    var vibrant by remember { mutableStateOf("#000000") }
+    var darkVibrant by remember { mutableStateOf("#000000") }
+    var onDarkVibrant by remember { mutableStateOf("#FFFFFF") }
+
+    LaunchedEffect(key1 = selectedHero){
+        vibrant = colors["vibrant"]!!
+        darkVibrant = colors["darkVibrant"]!!
+        onDarkVibrant = colors["onDarkVibrant"]!!
+    }
 
     val scaffoldState = rememberBottomSheetScaffoldState(
         bottomSheetState = rememberBottomSheetState(initialValue = BottomSheetValue.Expanded)
@@ -62,8 +72,13 @@ fun DetailsContent(
         scaffoldState = scaffoldState,
         sheetPeekHeight = 140.dp,
         sheetContent = {
-            if (selectedHero != null) {
-                BottomSheetContent(selectedHero = selectedHero)
+            selectedHero?.let {
+                BottomSheetContent(
+                    selectedHero = it,
+                    infoBoxIconColor = Color(parseColor(vibrant)),
+                    sheetBackgroundColor = Color(parseColor(darkVibrant)),
+                    contentColor = Color(parseColor(onDarkVibrant)),
+                )
             }
         },
         content = {
@@ -73,6 +88,7 @@ fun DetailsContent(
                     BackgroundContent(
                         heroImage = hero.image,
                         imageFraction = currentSheetFraction,
+                        backgroundColor = Color(parseColor(darkVibrant)),
                         onCloseClicked = {
                             navController.popBackStack()
                         }
--- app/src/main/java/com/example/borutoapp/presentation/details/DetailsScreen.kt
+++ app/src/main/java/com/example/borutoapp/presentation/details/DetailsScreen.kt
@@ -2,12 +2,19 @@ package com.example.borutoapp.presentation.details
 
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import dagger.hilt.android.lifecycle.HiltViewModel
+import coil.annotation.ExperimentalCoilApi
+import com.example.borutoapp.util.Constants.BASE_URL
+import com.example.borutoapp.util.PaletteGenerator.convertImageUrlToBitmap
+import com.example.borutoapp.util.PaletteGenerator.extractColorFromBitmap
+import kotlinx.coroutines.flow.collectLatest
 
+@ExperimentalCoilApi
 @ExperimentalMaterialApi
 @Composable
 fun DetailsScreen(
@@ -17,8 +24,44 @@ fun DetailsScreen(
 ){
 
     val selectedHero by detailsViewModel.selectedHero.collectAsState()
+    val colorPalette by detailsViewModel.colorPalette
 
+    if(colorPalette.isEmpty()) {
+        DetailsContent(
+            navController = navController,
+            selectedHero = selectedHero,
+            colors = colorPalette
+        )
 
-    DetailsContent(navController = navController, selectedHero = selectedHero)
+    } else {
+
+        detailsViewModel.generateColorPalette()
+    }
+
+    val context = LocalContext.current
+
+    LaunchedEffect(key1 = true ){
+        detailsViewModel.uiEvent.collectLatest { event ->
+
+            when(event){
+                is UiEvent.GenerateColorPalette -> {
+
+                    val bitmap = convertImageUrlToBitmap(
+                        imageUrl = "$BASE_URL${selectedHero?.image}",
+                        context = context
+                    )
+                    if(bitmap != null) {
+                        detailsViewModel.setColorPalette(
+                            colors = extractColorFromBitmap(
+                                bitmap = bitmap
+                            )
+                        )
+                    }
+
+                }
+            }
+        }
+    }
+
 
 }
